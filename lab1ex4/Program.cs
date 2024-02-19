f22bc11651b2a217767781e26711f2db9332f6f8
victor.gozu <victor.gozu@MP150159>
2024-02-19 18:43:27 +0200

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Ex 4
+Scrieti un program care va afisa semnul unui numar citit de la tastatura
+• Daca numarul este pozitiv, va afisa “+”
+• Daca numarul este negativ, va afisa “-”
+• Daca numarul este 0, va afisa “0”
+*/
+
+Console.WriteLine("Introduceti un numar intreg, fie pozitiv, fie negativ: ");
+int numarTastatura = int.Parse(Console.ReadLine());
+
+if (numarTastatura > 0)
+{
+    Console.WriteLine("+");
+}
+else if (numarTastatura < 0)
+{
+    Console.WriteLine("-");
+}
+else
+{
+    Console.WriteLine("0");
+}
